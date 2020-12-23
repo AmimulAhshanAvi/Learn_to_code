c500a5be2298bb8896a8202354c5147169d1aac9
avi <amimul.ahshan@cellosco.pe>
2020-12-24 01:27:21 +0600

    adding_some_exercise

---
+++ Assignment/Ch-4/pg-130/pg-130(eighties mega mix_.py
@@ -0,0 +1,17 @@
+eighties = ['', 'duran duran', 'B-52s' 'band_music']
+newwave = ['flock of seagulls', 'postal service']
+
+remember = eighties[1]
+
+eighties[1] ='culter club'
+
+band = newwave[0]
+
+eighties[3] = band
+
+eighties[0] = eighties[2]
+
+eighties[2] = remember
+
+print(eighties)
+
